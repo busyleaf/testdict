ddfd66985021a1e63d1f73016c182bdd7059be5c
busyleaf <rhsnipes@yahoo.com>
2017-07-14 14:50:53 -0700

    Initial Commit

---
+++ TestAstroDictionary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestAstroDictionary
+//
+//  Created by Robert Snipes on 7/14/17.
+//  Copyright © 2017 snyps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
